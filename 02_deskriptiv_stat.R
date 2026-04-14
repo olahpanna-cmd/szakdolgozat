77efc17d2a1fd9400382cb9af6046d7c6b6446c0
olahpanna-cmd <olahpanna@gmail.com>
2026-04-14 19:52:07 +0200

    Update 02_deskriptiv_stat.R

--- 02_deskriptiv_stat.R
+++ 02_deskriptiv_stat.R
@@ -1,25 +1,16 @@
-# --- Könyvtárak betöltése ---
 library(tidyverse)
 
-# --- Tisztított adatok beolvasása ---
 df <- readRDS("data/vegleges.rds")
 
-# ==============================================================================
-# DESKRIPTÍV STATISZTIKA
-# ==============================================================================
-
-# --- 1. A korpusz mérete ---
 osszes_beszed <- nrow(df)
 print(paste("A tisztított korpusz mérete:", osszes_beszed, "db felszólalás."))
 
-# --- 2. Ciklusonkénti megoszlás ---
 stat_ciklus <- df %>%
   count(electoral_cycle, name = "db")
 
 print("--- Felszólalások ciklusonként ---")
 print(stat_ciklus)
 
-# --- 3. Pártonkénti megoszlás ---
 stat_part <- df %>%
   count(party, name = "db") %>%
   arrange(desc(db)) 
